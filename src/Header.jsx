6b39adf8cb7c21103239538531d6fe0f9c316499
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-15 15:57:08 +1200

    Include AppBar, toolbar, and typrography components from material ui to build the header, also the coffee icon

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import FreeBreakfastIcon from '@material-ui/icons/FreeBreakfast';
 
 const Header = () => {
-  return <h1>This is the header</h1>;
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <Typography>Material UI sandbox - Coffee Shop</Typography>
+        <FreeBreakfastIcon />
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
