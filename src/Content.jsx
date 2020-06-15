4f48d368c3462bd8a8567099bdc33a7aeb3ce5db
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-15 17:16:56 +1200

    include breaking points to define the size of the items in xs and sm screens

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import CoffeeCard from './CoffeeCard';
+import { Grid } from '@material-ui/core';
 
 const Content = () => {
-  return <h1>This is our content</h1>;
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard />
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <CoffeeCard />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Content;
