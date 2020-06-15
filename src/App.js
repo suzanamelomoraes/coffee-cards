0b0b2f90f2582e234e090324e9338555a65e46dc
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-15 14:57:43 +1200

    create a basic grid structure to define the layout of the page such as columns and sizes of the items, header and content areas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 function App() {
   return (
-    <div>Coffee</div>
+    <Grid container direction='column'>
+      <Grid item>This will be the header</Grid>
+      <Grid item container>
+        <Grid item xs={false} sm={2} />
+        <Grid item xs={12} sm={8}>
+          This will be the content
+        </Grid>
+        <Grid item xs={false} sm={2} />
+      </Grid>
+    </Grid>
   );
 }
 
