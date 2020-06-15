d042053cd388ac1b47f008beb10bdb50c5ca9558
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-15 21:54:45 +1200

    Map array of data objects from data.js and send it to Coffee card as props

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import CoffeeCard from './CoffeeCard';
+import coffeeList from './data';
 import { Grid } from '@material-ui/core';
 
 const Content = () => {
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12} sm={4}>
-        <CoffeeCard />
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        <CoffeeCard />
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        <CoffeeCard />
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        <CoffeeCard />
-      </Grid>
+    <Grid container spacing={2}>
+      {coffeeList.map((coffee) => (
+        <Grid item xs={12} sm={4}>
+          <CoffeeCard {...coffee} />
+        </Grid>
+      ))}
     </Grid>
   );
 };
