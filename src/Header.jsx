ec9bfc74ba9bc6e82b53f930830d6c36343e2485
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-15 16:06:15 +1200

    apply style to update the flez to 1, by importing the makeStyles from material ui and apllying it into the typography as class with useStyles

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import FreeBreakfastIcon from '@material-ui/icons/FreeBreakfast';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(() => ({
+  typographyStyles: {
+    flex: 1,
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
   return (
     <AppBar position='static'>
       <Toolbar>
-        <Typography>Material UI sandbox - Coffee Shop</Typography>
+        <Typography className={classes.typographyStyles}>
+          Material UI sandbox - Coffee Shop
+        </Typography>
         <FreeBreakfastIcon />
       </Toolbar>
     </AppBar>
