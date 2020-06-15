d87cd6dcf2d19d1c06fae49296b1ad0693344e8b
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-15 15:15:55 +1200

    import and apply the Header component into the grid already defined to receive the header of the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './Header';
 import { Grid } from '@material-ui/core';
 
 function App() {
   return (
     <Grid container direction='column'>
-      <Grid item>This will be the header</Grid>
+      <Grid item>
+        <Header />
+      </Grid>
       <Grid item container>
         <Grid item xs={false} sm={2} />
         <Grid item xs={12} sm={8}>
