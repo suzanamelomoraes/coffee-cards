bb729fb1375a0899b9b71c50272c6a33b4155d3a
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-15 19:46:00 +1200

    delete unecessary items from the template such as classes and makeStyles

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
@@ -11,23 +10,7 @@ import IconButton from '@material-ui/core/IconButton';
 import ShareIcon from '@material-ui/icons/Share';
 import { CardMedia } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
 const CoffeeCard = (props) => {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
   const { avatarUrl, title, subtitle, description, imageUrl } = props;
 
   return (
