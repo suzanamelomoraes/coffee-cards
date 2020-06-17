8174f36a3cdf80dd1b6fac4a1b962540f72c87a0
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-17 16:42:40 +1200

    apply properties

--- src/Offers.jsx
+++ src/Offers.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 
 const Offers = () => {
-  return <Typography> Offer of the day </Typography>;
+  return (
+    <Typography color='primary' variant='h3'>
+      {' '}
+      Offer of the day{' '}
+    </Typography>
+  );
 };
 
 export default Offers;
