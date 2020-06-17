60e07552d49a044a5477cf539f3371dbc7bba2b0
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-06-17 16:40:33 +1200

    create a new component to shows the offer of the day

---
+++ src/Offers.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Offers = () => {
+  return <Typography> Offer of the day </Typography>;
+};
+
+export default Offers;
